a6df6129aa92026ee22f1c97802e9ac99d07d7d1
Van Beek Ben <ben_van_beek@hotmail.com>
2015-04-14 09:07:12 +0200

    "Bakkerij project toegevoegd + Demo uitvoerbaar - Bestellingoverzicht niet werkend"

---
+++ Bakkerij/src/Bestellingen/Data/DBConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Bestellingen\Data;
+
+class DBConfig {
+
+    public static $DB_Connstring = 'mysql:host=benvanbeek.com.mysql;dbname=benvanbeek_com';
+    public static $DB_Username = 'benvanbeek_com';
+    public static $DB_Password = 'FNRBBYdu';
+
+}
